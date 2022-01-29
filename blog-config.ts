7361d3ae18fabf7be01d98f0dd67a6989102087e
YoungChang Lee <apricotsoul@gmail.com>
2022-01-29 20:10:04 +0900

    feat: blog-config

---
+++ blog-config.ts
@@ -0,0 +1,15 @@
+export const title = 'p-iknow';
+export const description = '피아노의 개발 일지 입니다.';
+export const author = 'p-iknow';
+export const siteUrl = 'https://devhudi.github.io/gatsby-starter-hoodie/';
+export const links = {
+  github: 'https://github.com/devHudi',
+  linkedIn: 'https://linkedin.com',
+  facebook: 'https://www.facebook.com',
+  instagram: 'https://www.instagram.com',
+  email: 'mailto:devhudi@gmail.com',
+};
+export const utterances = {
+  repo: 'devHudi/gatsby-starter-hoodie',
+  type: 'pathname',
+};
