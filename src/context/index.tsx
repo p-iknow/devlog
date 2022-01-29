55cf15b8f031e035e9a75320fb4a7cc421170823
YoungChang Lee <apricotsoul@gmail.com>
2022-01-29 21:52:28 +0900

    feat: ThemeContext

---
+++ src/context/index.tsx
@@ -0,0 +1,29 @@
+import React, { createContext, useContext, useState } from 'react';
+
+export type Theme = 'light' | 'dark';
+
+export type SetTheme = (Theme: Theme) => void;
+
+export const ThemeContext = createContext<Theme | null>(null);
+export const SetThemeContext = createContext<SetTheme | null>(null);
+
+export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  const [theme, setTheme] = useState<Theme>('light');
+
+  <ThemeContext.Provider value={theme}>
+    <SetThemeContext.Provider value={setTheme}>{children}</SetThemeContext.Provider>
+  </ThemeContext.Provider>;
+};
+
+export const useThemeState = () => {
+  const theme = useContext(ThemeContext);
+  if (!theme) throw new Error('Cannot find ThemeProvider');
+  return theme;
+};
+
+export const useSetTheme = () => {
+  const setTheme = useContext(SetThemeContext);
+  if (!setTheme) throw new Error('Cannot find ThemeProvider');
+
+  return setTheme;
+};
