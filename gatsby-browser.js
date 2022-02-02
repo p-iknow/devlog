7aaca7aa09c9e43df58ffeb1171fae770adef89d
YoungChang Lee <apricotsoul@gmail.com>
2022-02-02 19:05:03 +0900

    feat: pretandard 폰트 적용

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 require("prismjs/themes/prism-tomorrow.css")
 require("katex/dist/katex.min.css")
+require("pretendard/dist/web/static/pretendard-dynamic-subset.css")
 import WrapRootElement from "./src/index";
 
 export const wrapRootElement = WrapRootElement
--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -6,7 +6,7 @@ import reset from 'styled-reset';
 const CustomStyles = createGlobalStyle`
 	${reset}
 	body {
-		font-family: 'Noto Sans KR', sans-serif;
+		font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', sans-serif;
 		background: ${props => props.theme.colors.bodyBackground};
 	}
 `;
