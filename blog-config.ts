a62c5d167c06978050c3f2c181a5125345696678
YoungChang Lee <apricotsoul@gmail.com>
2022-02-02 20:38:34 +0900

    feat: title 변경

--- blog-config.ts
+++ blog-config.ts
@@ -1,13 +1,13 @@
 const config = {
-  title: `🎹 p-iknow`,
+  title: `p-iknow's devlog`,
   description: '많은 것을 이해하고 싶습니다. 더 이해하기 위해 노력합니다.',
-  author: 'p-iknow',
+  author: 'p-iknow 🎹',
   siteUrl: 'https://p-iknow.netlify.app/',
   links: {
     github: 'https://github.com/p-iknow',
     linkedIn: 'https://www.linkedin.com/in/p-iknow/',
     facebook: 'https://www.facebook.com/devpiknow',
-    instagram: 'https://www.instagram.com',
+    instagram: 'https://www.instagram.com/_godot/',
     email: 'mailto:apricotsoul@gmail.com',
   },
   utterances: {
