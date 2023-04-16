e6e50f4c3bd5c7f10c601b718cfe328a3aec9ae3
p-iknow <apricotsoul@gmail.com>
2023-04-16 22:09:24 +0900

    feat: SeriesPage Head 업데이트

--- src/pages/series.tsx
+++ src/pages/series.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import { flow, map, groupBy, sortBy, filter, reverse } from 'lodash/fp';
 import styled from 'styled-components';
-import SEO from 'containers/SEO';
 
 import { graphql } from 'gatsby';
 
@@ -14,11 +13,35 @@ import NoContent from 'components/NoContent';
 
 import { blogConfig } from '../../blog-config';
 
-const TagListWrapper = styled.div`
-  margin-top: 20px;
-
-  @media (max-width: 768px) {
-    padding: 0 15px;
+export const pageQuery = graphql`
+  {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(
+      sort: { frontmatter: { date: DESC } }
+      filter: { frontmatter: { draft: { ne: true } } }
+    ) {
+      group(field: { frontmatter: { tags: SELECT } }) {
+        fieldValue
+        totalCount
+      }
+      nodes {
+        excerpt(pruneLength: 200, truncate: true)
+        fields {
+          slug
+        }
+        frontmatter {
+          date
+          update
+          title
+          tags
+          series
+        }
+      }
+    }
   }
 `;
 
@@ -88,51 +111,53 @@ const SeriesPage = ({ data }: Props) => {
 
   return (
     <Layout>
-      <SEO title={blogConfig.title} description={blogConfig.description} url={blogConfig.siteUrl} />
-
       <TagListWrapper>
         {series.length > 0 && <Title size="sm">There are {series.length} series.</Title>}
       </TagListWrapper>
-
       {series.length === 0 && <NoContent name="series" />}
-
       <VerticalSpace size={32} />
-
       <SeriesList seriesList={series} />
     </Layout>
   );
 };
 
-export default SeriesPage;
+const TagListWrapper = styled.div`
+  margin-top: 20px;
 
-export const pageQuery = graphql`
-  {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(
-      sort: { frontmatter: { date: DESC } }
-      filter: { frontmatter: { draft: { ne: true } } }
-    ) {
-      group(field: { frontmatter: { tags: SELECT } }) {
-        fieldValue
-        totalCount
-      }
-      nodes {
-        excerpt(pruneLength: 200, truncate: true)
-        fields {
-          slug
-        }
-        frontmatter {
-          date
-          update
-          title
-          tags
-          series
-        }
-      }
-    }
+  @media (max-width: 768px) {
+    padding: 0 15px;
   }
 `;
+
+export default SeriesPage;
+
+export const Head = ({ data }: Props) => {
+  const ogImgUrl = `${blogConfig.siteUrl}/og-img.jpeg`;
+  const title = data.site.siteMetadata.title;
+  const description = blogConfig.description;
+  const url = blogConfig.siteUrl + 'series/';
+  return (
+    <>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="author" content={blogConfig.author} />
+      {/* Facebook Meta Tags */}
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content={title} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={ogImgUrl} />
+      {/*  Twitter Meta Tags  */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="p-iknow.netlify.app" />
+      <meta property="twitter:url" content={url} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={ogImgUrl} />
+      <meta name="twitter:label1" content="Category" />
+      <meta name="twitter:data1" content="개발" />
+    </>
+  );
+};
