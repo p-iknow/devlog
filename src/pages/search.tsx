f399ac7d5dc68b50e9b5ddc632e664cf22bb5c58
p-iknow <apricotsoul@gmail.com>
2023-04-16 22:12:19 +0900

    feat: Search Page Header

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useMemo } from 'react';
 import styled from 'styled-components';
-import SEO from 'containers/SEO';
 import { graphql } from 'gatsby';
 
 import Layout from 'components/Layout';
@@ -10,10 +9,25 @@ import Title from 'components/Title';
 import VerticalSpace from 'components/VerticalSpace';
 import { blogConfig } from '../../blog-config';
 
-const SearchWrapper = styled.div`
-  margin-top: 20px;
-  @media (max-width: 768px) {
-    padding: 0 15px;
+export const pageQuery = graphql`
+  {
+    allMarkdownRemark(
+      sort: { frontmatter: { date: DESC } }
+      filter: { frontmatter: { draft: { ne: true } } }
+    ) {
+      nodes {
+        excerpt(pruneLength: 200, truncate: true)
+        fields {
+          slug
+        }
+        frontmatter {
+          date
+          title
+          tags
+        }
+        rawMarkdownBody
+      }
+    }
   }
 `;
 
@@ -56,43 +70,60 @@ const Search = ({ data }: Props) => {
   );
 
   return (
-    <Layout>
-      <SEO title={blogConfig.title} description={blogConfig.description} url={blogConfig.siteUrl} />
-      <SearchWrapper>
-        <Title size="sm">
-          There are {filteredPosts.length} post{filteredPosts.length > 1 && 's'}.
-        </Title>
-        <TextField
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setQuery(e.target.value)}
-          placeholder="Search"
-        />
-      </SearchWrapper>
-      <VerticalSpace size={70} />
-      <PostList postList={filteredPosts} />
-    </Layout>
+    <>
+      <Layout>
+        <SearchWrapper>
+          <Title size="sm">
+            There are {filteredPosts.length} post{filteredPosts.length > 1 && 's'}.
+          </Title>
+          <TextField
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setQuery(e.target.value)}
+            placeholder="Search"
+          />
+        </SearchWrapper>
+        <VerticalSpace size={70} />
+        <PostList postList={filteredPosts} />
+      </Layout>
+    </>
   );
 };
 
-export default Search;
-
-export const pageQuery = graphql`
-  {
-    allMarkdownRemark(
-      sort: { frontmatter: { date: DESC } }
-      filter: { frontmatter: { draft: { ne: true } } }
-    ) {
-      nodes {
-        excerpt(pruneLength: 200, truncate: true)
-        fields {
-          slug
-        }
-        frontmatter {
-          date
-          title
-          tags
-        }
-        rawMarkdownBody
-      }
-    }
+const SearchWrapper = styled.div`
+  margin-top: 20px;
+  @media (max-width: 768px) {
+    padding: 0 15px;
   }
 `;
+
+export default Search;
+
+export const Head = () => {
+  const ogImgUrl = `${blogConfig.siteUrl}/og-img.jpeg`;
+  const title = `p-iknow's dev-log search page`;
+  const description = `p-iknow's dev-log search page`;
+  const url = blogConfig.siteUrl + 'search/';
+  return (
+    <>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="author" content={blogConfig.author} />
+      {/* Facebook Meta Tags */}
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content={title} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={ogImgUrl} />
+      {/*  Twitter Meta Tags  */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="p-iknow.netlify.app" />
+      <meta property="twitter:url" content={url} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={ogImgUrl} />
+      <meta name="twitter:label1" content="Category" />
+      <meta name="twitter:data1" content="개발" />
+    </>
+  );
+};
