599f109964e21e84a192acd8f0bc3d1e08c3ad55
p-iknow <apricotsoul@gmail.com>
2023-04-16 21:50:54 +0900

    feat: tag page에 Head 적용

--- src/pages/tags.tsx
+++ src/pages/tags.tsx
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import _ from 'lodash';
 import styled from 'styled-components';
-import SEO from 'containers/SEO';
-import filter from 'lodash/filter';
 
 import { graphql } from 'gatsby';
 
@@ -16,6 +13,7 @@ import VerticalSpace from 'components/VerticalSpace';
 
 import isServer from 'utils/isServer';
 import { blogConfig } from '../../blog-config';
+import { sortBy } from 'remeda';
 
 const TagListWrapper = styled.div`
   margin-top: 20px;
@@ -56,58 +54,6 @@ interface Props {
     };
   };
 }
-
-const TagsPage = ({ data }: Props) => {
-  const tags = _.sortBy(data.allMarkdownRemark.group, ['totalCount']).reverse();
-  const posts = data.allMarkdownRemark.nodes;
-
-  const [selected, setSelected] = useState<string | undefined>();
-  const [filteredPosts, setFilteredPosts] = useState<Post[]>([]);
-
-  const query = isServer() ? '' : document?.location.search;
-  const q = queryString.parse(query)['q'] as string;
-
-  useEffect(() => {
-    if (!selected) {
-      setFilteredPosts(posts);
-      return;
-    }
-
-    setFilteredPosts(filter(posts, post => post.frontmatter.tags.indexOf(selected) !== -1));
-  }, [selected, posts]);
-
-  useEffect(() => {
-    setSelected(q);
-  }, [q]);
-
-  return (
-    <Layout>
-      <SEO title={blogConfig.title} description={blogConfig.description} url={blogConfig.siteUrl} />
-
-      <TagListWrapper>
-        {selected ? (
-          <Title size="sm">
-            There are {filteredPosts.length} post
-            {filteredPosts.length > 1 && 's'} that match #{selected}.
-          </Title>
-        ) : (
-          <Title size="sm">
-            There are {tags.length} tag{tags.length > 1 && 's'}.
-          </Title>
-        )}
-
-        <TagList count tagList={tags} selected={selected} />
-      </TagListWrapper>
-
-      <VerticalSpace size={32} />
-
-      <PostList postList={filteredPosts} />
-    </Layout>
-  );
-};
-
-export default TagsPage;
-
 export const pageQuery = graphql`
   {
     site {
@@ -138,3 +84,83 @@ export const pageQuery = graphql`
     }
   }
 `;
+
+const TagsPage = ({ data }: Props) => {
+  const tags = sortBy(data.allMarkdownRemark.group, v => v.totalCount).reverse();
+  const posts = data.allMarkdownRemark.nodes;
+
+  const [selected, setSelected] = useState<string | undefined>();
+  const [filteredPosts, setFilteredPosts] = useState<Post[]>([]);
+
+  const query = isServer() ? '' : document?.location.search;
+  const q = queryString.parse(query)['q'] as string;
+
+  useEffect(() => {
+    if (!selected) {
+      setFilteredPosts(posts);
+      return;
+    }
+
+    setFilteredPosts(posts.filter(post => post.frontmatter.tags.indexOf(selected) !== -1));
+  }, [selected, posts]);
+
+  useEffect(() => {
+    setSelected(q);
+  }, [q]);
+
+  return (
+    <>
+      <Layout>
+        <TagListWrapper>
+          {selected ? (
+            <Title size="sm">
+              There are {filteredPosts.length} post
+              {filteredPosts.length > 1 && 's'} that match #{selected}.
+            </Title>
+          ) : (
+            <Title size="sm">
+              There are {tags.length} tag{tags.length > 1 && 's'}.
+            </Title>
+          )}
+          <TagList count tagList={tags} selected={selected} />
+        </TagListWrapper>
+        <VerticalSpace size={32} />
+        <PostList postList={filteredPosts} />
+      </Layout>
+    </>
+  );
+};
+
+export default TagsPage;
+
+export const Head = ({ data }: Props) => {
+  const ogImgUrl = `${blogConfig.siteUrl}/og-img.jpeg`;
+  const title = data.site.siteMetadata.title;
+  const description = blogConfig.description;
+  const url = blogConfig.siteUrl + 'tags/';
+  return (
+    <>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="author" content={blogConfig.author} />
+
+      {/* Facebook Meta Tags */}
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content={title} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={ogImgUrl} />
+      {/*  Twitter Meta Tags  */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="p-iknow.netlify.app" />
+      <meta property="twitter:url" content={url} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={ogImgUrl} />
+      <meta name="twitter:label1" content="Category" />
+      <meta name="twitter:data1" content="개발" />
+    </>
+  );
+};
