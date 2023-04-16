fb7211c0626b7324924c31fed9402b8c02443347
p-iknow <apricotsoul@gmail.com>
2023-04-16 22:18:45 +0900

    feat: 404 페이지 Head

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Layout from 'components/Layout';
-import SEO from 'containers/SEO';
 import { blogConfig } from '../../blog-config';
 
 const NotFound = styled.div`
@@ -31,12 +30,43 @@ const NotFound = styled.div`
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title={blogConfig.title} description={blogConfig.description} url={blogConfig.siteUrl} />
     <NotFound>
-      <h2>404 ERROR</h2>
-      <h3>Page Not Found X(</h3>
+      <h2>페이지를 찾을 수 없습니다.</h2>
+      <h3>입력하신 주소에 해당하는 페이지가 없습니다. </h3>
     </NotFound>
   </Layout>
 );
 
 export default NotFoundPage;
+
+export const Head = () => {
+  const ogImgUrl = `${blogConfig.siteUrl}/og-img.jpeg`;
+  const title = `p-iknow's dev-log not found page`;
+  const description = title;
+  const url = blogConfig.siteUrl + '404/';
+  return (
+    <>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="author" content={blogConfig.author} />
+
+      {/* Facebook Meta Tags */}
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content={title} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={ogImgUrl} />
+      {/*  Twitter Meta Tags  */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="p-iknow.netlify.app" />
+      <meta property="twitter:url" content={url} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={ogImgUrl} />
+      <meta name="twitter:label1" content="Category" />
+      <meta name="twitter:data1" content="개발" />
+    </>
+  );
+};
