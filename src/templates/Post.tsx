28c09a6bc0f5606f6fc5e449f169c2f24e48125f
p-iknow <apricotsoul@gmail.com>
2023-04-16 21:51:41 +0900

    feat: Post page Head 적용

--- src/templates/Post.tsx
+++ src/templates/Post.tsx
@@ -3,111 +3,9 @@ import ArticleFooter from 'components/Article/Footer';
 import ArticleHeader from 'components/Article/Header';
 import ArticleSeries from 'components/Article/Series';
 import Layout from 'components/Layout';
-import SEO from 'containers/SEO';
 import { graphql } from 'gatsby';
 import React from 'react';
 import { blogConfig } from '../../blog-config';
-
-interface Props {
-  data: {
-    site: {
-      siteMetadata: {
-        title: string;
-      };
-    };
-    markdownRemark: {
-      id: string;
-      excerpt: string;
-      html: string;
-      frontmatter: {
-        title: string;
-        description?: string;
-        date: string;
-        update: string;
-        tags: string[];
-        series: string;
-        img?: string;
-      };
-      fields: {
-        slug: string;
-      };
-    };
-    seriesList: {
-      edges: {
-        node: {
-          id: string;
-          fields: {
-            slug: string;
-          };
-          frontmatter: {
-            title: string;
-          };
-        };
-      }[];
-    };
-    previous: {
-      fields: {
-        slug: '/writing-guide/';
-      };
-      frontmatter: {
-        title: '🤔 3. Writing Guide';
-      };
-    } | null;
-    next: {
-      fields: {
-        slug: '/writing-guide/';
-      };
-      frontmatter: {
-        title: '🤔 3. Writing Guide';
-      };
-    } | null;
-  };
-}
-
-const Post = ({ data }: Props) => {
-  const post = data.markdownRemark;
-  const { previous, next, seriesList } = data;
-
-  const { title, date, tags, series, img, description } = post.frontmatter;
-  const { excerpt } = post;
-  const { slug } = post.fields;
-
-  const filteredSeries = series
-    ? seriesList.edges.map(seriesPost => {
-        if (seriesPost.node.id === post.id) {
-          return {
-            ...seriesPost.node,
-            currentPost: true,
-          };
-        } else {
-          return {
-            ...seriesPost.node,
-            currentPost: false,
-          };
-        }
-      })
-    : [];
-
-  return (
-    <Layout>
-      <SEO
-        title={title}
-        description={description ?? excerpt}
-        url={`${blogConfig.siteUrl}${slug}`}
-        ogImg={img}
-        keywords={tags}
-      />
-      <article>
-        <ArticleHeader title={title} date={date} tags={tags} />
-        {filteredSeries.length > 0 && <ArticleSeries header={series} series={filteredSeries} />}
-        <ArticleBody html={post.html} />
-        <ArticleFooter previous={previous} next={next} />
-      </article>
-    </Layout>
-  );
-};
-
-export default Post;
 
 export const pageQuery = graphql`
   query BlogPostBySlug(
@@ -172,3 +70,130 @@ export const pageQuery = graphql`
     }
   }
 `;
+
+interface Props {
+  data: {
+    site: {
+      siteMetadata: {
+        title: string;
+      };
+    };
+    markdownRemark: {
+      id: string;
+      excerpt: string;
+      html: string;
+      frontmatter: {
+        title: string;
+        description?: string;
+        date: string;
+        update: string;
+        tags: string[];
+        series: string;
+        img?: string;
+      };
+      fields: {
+        slug: string;
+      };
+    };
+    seriesList: {
+      edges: {
+        node: {
+          id: string;
+          fields: {
+            slug: string;
+          };
+          frontmatter: {
+            title: string;
+          };
+        };
+      }[];
+    };
+    previous: {
+      fields: {
+        slug: '/writing-guide/';
+      };
+      frontmatter: {
+        title: '🤔 3. Writing Guide';
+      };
+    } | null;
+    next: {
+      fields: {
+        slug: '/writing-guide/';
+      };
+      frontmatter: {
+        title: '🤔 3. Writing Guide';
+      };
+    } | null;
+  };
+}
+
+const Post = ({ data }: Props) => {
+  const post = data.markdownRemark;
+  const { previous, next, seriesList } = data;
+
+  const { title, date, tags, series } = post.frontmatter;
+
+  const filteredSeries = series
+    ? seriesList.edges.map(seriesPost => {
+        if (seriesPost.node.id === post.id) {
+          return {
+            ...seriesPost.node,
+            currentPost: true,
+          };
+        } else {
+          return {
+            ...seriesPost.node,
+            currentPost: false,
+          };
+        }
+      })
+    : [];
+
+  return (
+    <Layout>
+      <article>
+        <ArticleHeader title={title} date={date} tags={tags} />
+        {filteredSeries.length > 0 && <ArticleSeries header={series} series={filteredSeries} />}
+        <ArticleBody html={post.html} />
+        <ArticleFooter previous={previous} next={next} />
+      </article>
+    </Layout>
+  );
+};
+
+export default Post;
+
+export const Head = ({ data }: Props) => {
+  const post = data.markdownRemark;
+  const { title, tags, img, description } = post.frontmatter;
+  const { slug } = post.fields;
+  const ogImgUrl = img;
+  const url = `${blogConfig.siteUrl}${slug}`;
+  const metaDescription = description ?? post.excerpt;
+  const keywords = tags.join(',');
+  return (
+    <>
+      <title>{title}</title>
+      <meta name="description" content={metaDescription} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="author" content={blogConfig.author} />
+      {keywords && <meta name="keywords" content={keywords} />}
+      {/* Facebook Meta Tags */}
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content={title} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={metaDescription} />
+      <meta property="og:image" content={ogImgUrl} />
+      {/*  Twitter Meta Tags  */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="p-iknow.netlify.app" />
+      <meta property="twitter:url" content={url} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={metaDescription} />
+      <meta name="twitter:image" content={ogImgUrl} />
+      <meta name="twitter:label1" content="Category" />
+      <meta name="twitter:data1" content="개발" />
+    </>
+  );
+};
