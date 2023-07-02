522700b265a20f4052c9401100ae1e30028a36bd
p-iknow <apricotsoul@gmail.com>
2023-07-02 20:11:34 +0900

    feat: delete ssr

--- gatsby-ssr.tsx
+++
@@ -1,7 +0,0 @@
-import type { WrapPageElementNodeArgs } from 'gatsby';
-import React from 'react';
-import Root from './src';
-
-export const wrapPageElement = ({ element }: WrapPageElementNodeArgs) => {
-  return <Root>{element}</Root>;
-};
